8953928402494667f070e17d36ceb20a93f50097
Einar Norðfjörð <einar@nordfjord.io>
2022-11-26 23:39:50 -0500

    Add fish plugins

--- nvim/lua/plugin/treesitter.lua
+++ nvim/lua/plugin/treesitter.lua
@@ -1,5 +1,5 @@
 require('nvim-treesitter.configs').setup({
-  ensure_installed = { 'lua', 'ocaml', 'markdown', 'python', 'toml', 'yaml', 'vim' },
+  ensure_installed = { 'lua', 'ocaml', 'markdown', 'python', 'toml', 'yaml', 'vim', 'fish' },
   highlight = {
     enable = true,
   },
