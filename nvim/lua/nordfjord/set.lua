df0b8bc52be12ca0d9909fd33054a8005d8f6acd
Einar Norðfjörð <einar@nordfjord.io>
2023-03-26 23:10:58 -0400

    fix tmux colors

--- nvim/lua/nordfjord/set.lua
+++ nvim/lua/nordfjord/set.lua
@@ -1,9 +1,11 @@
 vim.opt.number = true
 vim.opt.relativenumber = true
 
-vim.opt.tabstop = 4
-vim.opt.softtabstop = 4
-vim.opt.shiftwidth = 4
+local tabwidth = 2
+
+vim.opt.tabstop = tabwidth
+vim.opt.softtabstop = tabwidth
+vim.opt.shiftwidth = tabwidth
 vim.opt.expandtab = true
 vim.opt.smartindent = true
 
@@ -17,6 +19,7 @@ vim.opt.undofile = true
 vim.opt.hlsearch = true
 vim.opt.incsearch = true
 vim.opt.smartcase = true
+vim.opt.ignorecase = true
 
 vim.opt.termguicolors = true
 
