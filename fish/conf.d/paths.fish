602340cd97c4e48411600ad989a4fa2963240942
Einar Norðfjörð <einar@nordfjord.io>
2023-04-03 08:50:59 -0400

    update keybindings for harpoon

--- fish/conf.d/paths.fish
+++ fish/conf.d/paths.fish
@@ -1,4 +1,5 @@
 fish_add_path /usr/local/bin
 fish_add_path $HOME/Library/pnpm
+fish_add_path $HOME/.cargo/bin
 set fish_user_paths ./node_modules/.bin $fish_user_paths 
 
--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -77,7 +77,7 @@ lsp.on_attach(function(client, bufnr)
     vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
     vim.keymap.set("n", "<leader>ca", function() vim.lsp.buf.code_action() end, opts)
     vim.keymap.set("n", "<leader>rn", function() vim.lsp.buf.rename() end, opts)
-    vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
+    vim.keymap.set("i", "<leader>h", function() vim.lsp.buf.signature_help() end, opts)
 end)
 
 
