28b08154fc8f9393cdd38df60912ff4c665fcb01
Einar Norðfjörð <gloogankle@gmail.com>
2023-11-10 17:47:38 -0500

    remove dead after plugins

--- nvim/_after/plugin/test.lua
+++
@@ -1,5 +0,0 @@
-vim.keymap.set("n", "<leader>tt", "<cmd>TestNearest<cr>")
-vim.keymap.set("n", "<leader>tf", "<cmd>TestFile<CR>")
-vim.keymap.set("n", "<leader>ts", "<cmd>TestSuite<CR>")
-vim.keymap.set("n", "<leader>tl", "<cmd>TestLast<CR>")
-vim.keymap.set("n", "<leader>to", "<cmd>TestVisit<CR>")
