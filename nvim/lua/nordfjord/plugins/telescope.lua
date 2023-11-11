8edeea7bd160b1514f2e73570ca873df275ca491
Einar Norðfjörð <gloogankle@gmail.com>
2023-11-10 22:54:10 -0500

    git blame shortcut

--- nvim/lua/nordfjord/plugins/telescope.lua
+++ nvim/lua/nordfjord/plugins/telescope.lua
@@ -1,17 +1,15 @@
 return {
   "nvim-telescope/telescope.nvim",
   branch = "0.1.x",
-  dependencies = {
-    { "nvim-lua/plenary.nvim" },
-  },
+  dependencies = { "nvim-lua/plenary.nvim" },
   config = function()
     require("telescope").setup({
       defaults = {
         path_display = { "smart" }
       }
     })
-    local builtin = require("telescope.builtin")
 
+    local builtin = require("telescope.builtin")
     vim.keymap.set("n", "<leader>pf", builtin.find_files, {})
     vim.keymap.set("n", "<C-p>", builtin.git_files, {})
     vim.keymap.set("n", "<leader>bf", builtin.buffers, {})
--- nvim/lua/nordfjord/plugins/vim-extensions.lua
+++ nvim/lua/nordfjord/plugins/vim-extensions.lua
@@ -3,6 +3,7 @@ return {
     "tpope/vim-fugitive",
     config = function()
       vim.keymap.set("n", "<leader>gs", ":Git<CR>", { noremap = true, silent = true })
+      vim.keymap.set("n", "<leader>gb", ":Git blame<CR>", { noremap = true, silent = true })
     end
   },
   { "tpope/vim-surround" }
