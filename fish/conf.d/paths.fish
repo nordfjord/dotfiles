50dcbb25ac47fffa0b40912331a7b12bd7806531
Einar Nordfjord <gloogankle@gmail.com>
2024-04-09 23:56:17 -0400

    linux rice

--- fish/conf.d/paths.fish
+++ fish/conf.d/paths.fish
@@ -1,8 +1,10 @@
+set -x GOPATH $HOME/.local/share/gobin
 fish_add_path /usr/local/bin
 fish_add_path $HOME/Library/pnpm
 fish_add_path $HOME/.cargo/bin
 fish_add_path $HOME/.linkerd2/bin
 fish_add_path $HOME/.asdf/shims
 fish_add_path $HOME/go/bin
+fish_add_path $HOME/.local/share/gobin
 set fish_user_paths ./node_modules/.bin $fish_user_paths 
 
